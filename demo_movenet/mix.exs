1a5a15772dbea8476b060103cc9a5724c706aaca
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2022-12-19 08:45:20 +0900

    update

--- demo_movenet/mix.exs
+++ demo_movenet/mix.exs
@@ -25,7 +25,6 @@ defmodule DemoMovenet.MixProject do
       {:tfl_interp, path: ".."},
       {:nx, "~> 0.4.0"},
       {:cimg, "~> 0.1.15"},
-      {:postdnn, "~> 0.1.4"},
       {:exprintf, "~> 0.2.1"}
     ]
   end
