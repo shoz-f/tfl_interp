8c810ad2d489505d95a7dccedc41bb5575eba59b
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2023-04-12 08:01:11 +0900

    add progress bar to model downloader.

--- lib/tfl_interp/url.ex
+++ lib/tfl_interp/url.ex
@@ -49,7 +49,7 @@ defmodule TflInterp.URL do
     end
   end
 
-  defp get!(url) do
+  def get!(url) do
     http_opts = [
       ssl: [
         verify: :verify_peer,
@@ -60,19 +60,47 @@ defmodule TflInterp.URL do
       ]
     ]
 
-    case :httpc.request(:get, {url, []}, http_opts, body_format: :binary) do
-      {:ok, {{_, status, _}, headers, body}} ->
-        if status >= 400 do
-          raise "HTTP #{status} #{inspect(body)}"
-        else
-          %{status: status, headers: headers, body: body}
-        end
-
+    case :httpc.request(:get, {url, []}, http_opts, stream: :self, sync: false) do
+      {:ok, request_id} ->
+        get_loop(request_id, [], &ProgressBar.render/2)
       {:error, reason} ->
         raise inspect(reason)
     end
   end
   
+  defp get_loop(id, downloaded, progress \\ nil) do
+    receive do
+      {:http, reply_info} when elem(reply_info, 0) == id ->
+        case Tuple.delete_at(reply_info, 0) do
+          {:stream_start, headers} ->
+            get_loop(id, downloaded, init_progress(progress, headers))
+          {:stream, body} ->
+            get_loop(id, [body|downloaded], render_progress(progress, body))
+          {:stream_end, headers} ->
+            %{headers: headers, body: IO.iodata_to_binary(Enum.reverse(downloaded))}
+          {{status, _, _}} ->
+            status
+          any -> any
+        end
+    end
+  end
+
+  defp init_progress(render, headers) when is_function(render) do
+    {_, length} = List.keyfind!(headers, 'content-length', 0)
+    render = fn x -> render.(x, List.to_integer(length)) end
+
+    {0, render}
+  end
+  defp init_progress(progress, _), do: progress
+
+  defp render_progress({last_count, render}, bin) when is_function(render) do
+    count = last_count + byte_size(bin)
+    render.(count)
+
+    {count, render}
+  end
+  defp render_progress(progress, _), do: progress
+
   defp attachment_filename(headers) do
     with {_, cd} <- List.keyfind(headers, 'content-disposition', 0),
          [[_, fname]] <- Regex.scan(~r/filename="?(.+)"?/, List.to_string(cd))
