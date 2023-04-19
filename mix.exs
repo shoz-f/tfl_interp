947f0509f7b38e62556ca2f3fec7fcddf3424533
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2023-04-20 04:59:43 +0900

    for publish.

--- mix.exs
+++ mix.exs
@@ -86,7 +86,7 @@ defmodule TflInterp.MixProject do
        name: "tfl_interp",
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/shoz-f/tfl_interp.git"},
-      files: ~w(lib mix.exs README* CHANGELOG* LICENSE* CMakeLists.txt msys2.* *.cmake src)
+      files: ~w(lib mix.exs README* CHANGELOG* LICENSE* CMakeLists.txt *.patch *.cmake src)
     ]
   end
 
