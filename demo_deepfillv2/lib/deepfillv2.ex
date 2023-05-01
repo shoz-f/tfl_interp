217a7c9750e37cf1a12fbd495bbb46a323582666
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2023-05-01 22:00:32 +0900

    revised.

--- demo_deepfillv2/lib/deepfillv2.ex
+++ demo_deepfillv2/lib/deepfillv2.ex
@@ -17,10 +17,13 @@ defmodule DeepFillV2 do
       |> CImg.to_binary(range: {0.0, 255.0})
 
     # prediction
+    {w, h, _, _} = CImg.shape(img)
+
     session()
       |> NNInterp.set_input_tensor(0, input0)
       |> NNInterp.invoke()
       |> NNInterp.get_output_tensor(0)
       |> CImg.from_binary(@width, @height, 1, 3, [{:dtype, "<u1"}, :bgr])
+      |> CImg.resize({w, h})
   end
 end
