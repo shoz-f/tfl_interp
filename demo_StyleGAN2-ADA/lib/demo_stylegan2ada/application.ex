03a33a218f649e9c380b7667000abe9db88537d4
Shozo Fukuda <fukuda_shozo@yahoo.co.jp>
2023-06-21 18:46:53 +0900

    update demo_StyleGAN2-ADA.

--- demo_StyleGAN2-ADA/lib/demo_stylegan2ada/application.ex
+++ demo_StyleGAN2-ADA/lib/demo_stylegan2ada/application.ex
@@ -8,7 +8,8 @@ defmodule DemoStyleGAN2ADA.Application do
   @impl true
   def start(_type, _args) do
     children = [
-      StyleGAN2ADA
+      StyleGAN2ADA.Mapping,
+      StyleGAN2ADA.Synthesis
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
